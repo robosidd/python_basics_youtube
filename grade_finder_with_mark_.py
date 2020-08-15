f0aec5b660624cd6b064a332a045607ff806f3fc
robosidd <robosidd@gmail.com>
2020-08-15 18:59:23 +0530

    grade calculator
    This file is for learning if conditions by printing the corresponding grades to its marks.

---
+++ grade_finder_with_mark_.py
@@ -0,0 +1,9 @@
+marks = int(input("Enter the marks you got from 1 to 100 : "))
+if(marks>=80 and marks<100):
+    print("You have got A+ Grade")
+if (marks>=60 and marks<80):
+    print("You have got A Grade")
+if (marks>=40 and marks<60):
+    print("You have got B Grade")
+if (marks>=20 and marks<40):
+    print("You have failed !!")
